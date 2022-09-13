8e6447459016f2704f5cb64adbe12df43765dc9e
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-09-14 07:20:18 +0900

    학교 가기 전에 react-draggable 설치

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,65 @@
 import React from "react";
 import styled from "styled-components";
 import PageTransition from "../components/PageTransition";
+import { DiGithubFull } from "react-icons/di";
+import { SiKakao } from "react-icons/si";
+import { GrMedium } from "react-icons/gr";
 
 const ContactWrapper = styled.section`
-  width: 100vw;
   position: absolute;
-  left: 50%;
   top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const IconsWrapper = styled.ul`
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+
+  li {
+    cursor: pointer;
+  }
 `;
 
 const Contact = () => {
   return (
     <PageTransition>
-      <ContactWrapper>Contact</ContactWrapper>
+      <ContactWrapper>
+        <h1>
+          계속해서 앞으로 나아가는 개발자 <span>김태현</span>이 되겠습니다.
+        </h1>
+        <IconsWrapper>
+          <li>
+            <a
+              href="https://github.com/tjdrkr2580"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {<DiGithubFull size={64} />}
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/tjdrkr2580"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {<SiKakao size={64} />}
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/tjdrkr2580"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {<GrMedium size={36} />}
+            </a>
+          </li>
+        </IconsWrapper>
+      </ContactWrapper>
     </PageTransition>
   );
 };
